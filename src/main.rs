de4094db94807b1721a2c1d1c0663f4ae798a6d6
Aatif Syed <aatif.syed@metaswitch.com>
2021-05-12 17:09:15 +0100

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use noise;
-use noisimation::PrintImage;
+use itertools_num::linspace;
+use noise::{self};
+use noisimation::PrintImages;
 use structopt::StructOpt;
 use strum::VariantNames;
 use strum_macros::{Display as EnumDisplay, EnumString, EnumVariantNames};
@@ -8,19 +9,43 @@ use strum_macros::{Display as EnumDisplay, EnumString, EnumVariantNames};
 struct Opt {
     #[structopt(possible_values = FilterOpt::VARIANTS)]
     function: FilterOpt,
+    #[structopt(short, long)]
+    bias: f64,
+    #[structopt(short, long)]
+    scale: f64,
+    #[structopt(short, long)]
+    width: u32,
+    #[structopt(short, long)]
+    height: u32,
+    #[structopt(short, long)]
+    floor: f64,
+    #[structopt(short, long)]
+    ceiling: f64,
+    #[structopt(short = "n", long)]
+    slices: usize,
 }
 
 #[derive(Debug, EnumDisplay, EnumString, EnumVariantNames)]
 #[strum(serialize_all = "snake_case")]
 enum FilterOpt {
+    Billow(noise::Billow),
     Checkerboard(noise::Checkerboard),
+    Fbm(noise::Fbm),
+    OpenSimplex(noise::OpenSimplex),
+    Perlin(noise::Perlin),
+    SuperSimpex(noise::SuperSimplex),
     Value(noise::Value),
 }
 
-impl noise::NoiseFn<[f64; 3]> for FilterOpt {
+impl noise::NoiseFn<[f64; 3]> for Box<FilterOpt> {
     fn get(&self, point: [f64; 3]) -> f64 {
-        match self {
+        match self.as_ref() {
+            FilterOpt::Billow(f) => f.get(point),
             FilterOpt::Checkerboard(f) => f.get(point),
+            FilterOpt::Fbm(f) => f.get(point),
+            FilterOpt::OpenSimplex(f) => f.get(point),
+            FilterOpt::Perlin(f) => f.get(point),
+            FilterOpt::SuperSimpex(f) => f.get(point),
             FilterOpt::Value(f) => f.get(point),
         }
     }
@@ -28,6 +53,17 @@ impl noise::NoiseFn<[f64; 3]> for FilterOpt {
 fn main() -> std::result::Result<(), ()> {
     let opt = Opt::from_args();
     println!("{:#?}", opt);
-    let filter = &opt.function as &dyn noise::NoiseFn<[f64; 3]>;
+    let function = Box::new(opt.function);
+    let function = noise::ScaleBias::new(&function)
+        .set_bias(opt.bias)
+        .set_scale(opt.scale);
+    noisimation::make_volume(
+        &function,
+        opt.width,
+        opt.height,
+        linspace(opt.floor, opt.ceiling, opt.slices),
+    )
+    .map(|i| image::DynamicImage::ImageLuma16(i))
+    .print_images(&Default::default());
     Ok(())
 }
